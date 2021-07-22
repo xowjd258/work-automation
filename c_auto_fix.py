a51231e957a981d5f636d160dcfd0a69241848dc
xowjd258 <55902342+xowjd258@users.noreply.github.com>
2021-07-22 17:38:11 +0900

    Update c_auto_fix.py

--- c_auto_fix.py
+++ c_auto_fix.py
@@ -27,3 +27,38 @@ def updateCount():
         if i%100 == 0:
             print("{}번째 도는 중".format(i))
     mean_of_data = numpy.mean(lines)
+    var_of_data = numpy.var(lines)
+    code_arr = []
+    code_arr.append(str(mean_of_data))
+    code_arr.append(str(var_of_data))
+
+    #code = '\n'.join(code_arr)
+    code = ' '.join(code_arr)
+
+    fout = open("/home/9824/darknet/test/result2", 'w')
+    fout.write(code)
+    fout.close()
+
+def updateCount_self():
+    fin = open(__file__, 'r')
+    code = fin.read()
+    fin.close()
+
+    second_line = code.split('\n')[1]
+    second_line_parts = second_line.split(' ')
+    second_line_parts[2] = str(int(second_line_parts[2])+1)
+
+    second_line = ' '.join(second_line_parts)
+    lines = code.split('\n')
+    lines[1] = second_line
+    code = '\n'.join(lines)
+
+    fout = open(__file__, 'w')
+    fout.write(code)
+    fout.close()
+
+if __name__ == '__main__':
+   #print('This script has run {} times'.format(COUNT))
+    updateCount()
+    #updateCount_self()
+    
